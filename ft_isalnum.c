131ed1d8626dd5293f77d18846749c04b9fbed24
Houda Harit <hharit@e1r1p1.1337.ma>
2021-11-03 16:55:29 +0100

    edited

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,24 +6,10 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 09:56:56 by hharit            #+#    #+#             */
-/*   Updated: 2021/11/02 09:56:58 by hharit           ###   ########.fr       */
+/*   Updated: 2021/11/03 16:49:52 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= 0 && c <= 9)
-		return (1);
-	return (0);
-}
-
+#include "libft.h"
 int	ft_isalnum(int c)
 {
 	if (ft_isdigit(c) || ft_isdigit(c))
--- libft.h
+++ libft.h
@@ -1,5 +1,6 @@
-#define LIBFT_H;
-#ifndef LIBFT_H;
+#ifndef LIBFT_H
+#define LIBFT_H
 typedef unsigned int size_t;
-
+int	ft_isalpha(int);
+int	ft_isdigit(int);
 #endif
