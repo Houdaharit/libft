f7aaf1e8dd37af6f4b3117facdc3c2eebc9ee3f7
Houdaharit <61919194+Houdaharit@users.noreply.github.com>
2021-11-05 17:07:00 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,18 +12,74 @@
 
 #include "libft.h"
 
-static int	ft_lenstr(char const *s1, char const *set)
+static int ft_issep(char c, char const *set)
 {
-	int	len;
-	int	i;
+	int i;
 
 	i = 0;
-	len_s1 = ft_strlen(s1);
-	len_set = ft_strlen(set);
-	while (len && *s1 == set[--len_set])
-	{
-		ptr = s1;
-		s1++;
-	}
-
+	while (set[i] && c != set[i])
+		i++;
+	if(set[i])
+		return (1);
+	return (0);
 }
+
+static char const *ft_endstr(char const *s1, char const *set)
+{
+	int len;
+
+	len = 0;
+	while (s1[len])
+		len ++;
+	while (len && ft_issep(s1[len - 1], set))
+		len --;
+	return (s1 + len);
+}
+ 
+static int ft_lenstr(char const *s1, char const *set)
+{
+	char const	*ptr;
+	int len;
+
+	ptr = ft_endstr(s1, set);
+	len = 0;
+	while (*s1 && ft_issep(*s1,set))
+		s1++;
+	while (*s1 && s1 != ptr)
+	{
+		s1++;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int i;
+	char const *ptr;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_lenstr(s1,set) + 1));
+	if (!str)
+		return (NULL);
+	ptr = ft_endstr(s1, set);
+	while (*s1 && ft_issep(*s1,set))
+		s1++;
+	while (s1[i] && &s1[i] != ptr)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+	char const *str = "-*hello****everyone******-";
+	char const *set = "*-/he";
+	printf("%s",ft_strtrim(str, set));
+}*/
