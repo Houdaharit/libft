6b96d46c20244cc0d1be2dfe1875c474b138cad2
Houda Harit <hharit@e1r10p2.1337.ma>
2021-11-05 18:41:49 +0100

    init

---
+++ ft_strjoin.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 18:01:53 by hharit            #+#    #+#             */
+/*   Updated: 2021/11/05 18:38:21 by hharit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_length(char const *s1, char const *s2)
+{
+	int	len;
+
+	len = 0;
+	while (s1[len])
+		len ++;
+	while (*s2)
+	{
+		len ++;
+		s2 ++;
+	}
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_length(s1, s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (*s2)
+	{
+		str[i] = *s2;
+		i++;
+		s2 ++;
+	}
+	str[i] = *s2;
+	return (str);
+}
+/*
+#include <stdio.h>
+int main()
+{
+	printf("%s", ft_strjoin("hello ","everyone"));
+}*/
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,24 +6,77 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 12:26:15 by hharit            #+#    #+#             */
-/*   Updated: 2021/11/05 12:49:54 by hharit           ###   ########.fr       */
+/*   Updated: 2021/11/05 17:58:35 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_lenstr(char const *s1, char const *set)
+static int	ft_issep(char c, char const *set)
 {
-	int	len;
 	int	i;
 
 	i = 0;
-	len_s1 = ft_strlen(s1);
-	len_set = ft_strlen(set);
-	while (len && *s1 == set[--len_set])
-	{
-		ptr = s1;
-		s1++;
-	}
-
+	while (set[i] && c != set[i])
+		i++;
+	if (set[i])
+		return (1);
+	return (0);
 }
+
+static char	const	*ft_endstr(char const *s1, char const *set)
+{
+	int	len;
+
+	len = 0;
+	while (s1[len])
+		len ++;
+	while (len && ft_issep(s1[len - 1], set))
+		len --;
+	return (s1 + len);
+}
+
+static int	ft_lenstr(char const *s1, char const *set)
+{
+	char const	*ptr;
+	int			len;
+
+	ptr = ft_endstr(s1, set);
+	len = 0;
+	while (*s1 && ft_issep(*s1, set))
+		s1++;
+	while (s1[len] && &s1[len] != ptr)
+		len++;
+	return (len);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int			i;
+	char const	*ptr;
+	char		*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_lenstr(s1, set) + 1));
+	if (!str)
+		return (NULL);
+	ptr = ft_endstr(s1, set);
+	while (*s1 && ft_issep(*s1, set))
+		s1++;
+	while (s1[i] && &s1[i] != ptr)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+	char const *str = "hello****everyone******-";
+	char const *set = "he*-";
+	printf("%s",ft_strtrim(str, set));
+}*/
