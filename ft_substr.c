3f988353575334b5b16dc989adb556a8f9438799
Houda Harit <hharit@e1r10p2.1337.ma>
2021-11-05 20:23:02 +0100

    init

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 19:59:16 by hharit            #+#    #+#             */
+/*   Updated: 2021/11/05 20:22:18 by hharit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t		i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len && s[start + i])
+	{
+		str[i] = s[start + i];
+		i ++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	printf("%s", ft_substr("hello everyone",6,11));
+}*/
