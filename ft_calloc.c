0b063fc89d1b45cf387418dcb82f0fcfce057810
Houda Harit <hharit@e1r5p6.1337.ma>
2021-11-07 16:03:03 +0100

    init

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 09:20:57 by hharit            #+#    #+#             */
+/*   Updated: 2021/11/07 16:01:14 by hharit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*object;
+
+	object = malloc(size * count);
+	if (!object)
+		return (NULL);
+	ft_bzero(object, 0);
+	return (object);
+}
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,31 +6,32 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 13:04:57 by hharit            #+#    #+#             */
-/*   Updated: 2021/11/05 19:44:16 by hharit           ###   ########.fr       */
+/*   Updated: 2021/11/07 10:32:46 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <string.h>
+
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
+
 {
 	size_t		i;
-	const char	*str1;
-	const char	*str2;
+	unsigned  const char	*str1;
+	unsigned  const char	*str2;
 
 	str1 = s1;
 	str2 = s2;
 	i = 0;
-	
 	while (i < n)
 	{
-		if (*(unsigned char*)str1++ - *(unsigned char*)str2++)
-			return (*(unsigned char*)str1++ - *(unsigned char*)str2++);
+		if ((str1 + i) - *(str2+ i))
+			return (*(str1 + i) - *(str2 + i));
 		i++;
 	}
 	return (0);
-	
 }
+
 #include <stdio.h>
+#include <string.h>
 int main()
 {
 	const char str1[10] = "1cvscsk";
