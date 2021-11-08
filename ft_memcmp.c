6e12b4bd7f7f0807c5f83cf05df4eec800779188
Houda Harit <hharit@e2r9p2.1337.ma>
2021-11-08 17:41:13 +0100

    init

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hharit <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 13:04:57 by hharit            #+#    #+#             */
-/*   Updated: 2021/11/07 16:06:28 by hharit           ###   ########.fr       */
+/*   Updated: 2021/11/08 17:39:13 by hharit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,33 +14,23 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 {
-	size_t		i;
-	unsigned  const char	*str1;
-	unsigned  const char	*str2;
+	size_t	i;
 
-	str1 = s1;
-	str2 = s2;
 	i = 0;
-	while (i < n)
-	{
-		if ((str1 + i) - *(str2+ i))
-			return (*(str1 + i) - *(str2 + i));
-		i++;
-	}
-	return (0);
-<<<<<<< HEAD
+	while (i < n && *(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i))
+		i ++;
+	if (i == n)
+		return (0);
+	return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
 }
-
-=======
-	
-}/*
+/*
 #include <stdio.h>
 #include <string.h>
 int main()
 {
 	const char str1[10] = "1cvscsk";
 	const char str2[10] = "9cgsucsi";
-	printf("%d\n", ft_memcmp(str1, str2, 5));
-}
-}*/
+	printf("orginal :%d\n", (memcmp(str1, str2, 5));
+	printf("ft : %d\n", (ft_memcmp(str1,str2,5));
 
+}*/
